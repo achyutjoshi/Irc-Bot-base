37daac7699d478d52d9634620b1ac4a13ae85dea
NDJ <ndj@ndjwabsite.com>
2014-01-22 04:55:53 -0500

    Began work

---
+++ Main.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#Filename: Main.py
+import ircfunctions
+import ssl
+import socket
+# The following lines, are for setting the constants that the irc bot
+# will connect to.
+OWNER = "NDJ" #Set a bot owner, who can reset and control bot
+HOST = "irc.rizon.net" #All the constants used here, can be edited, this is simply for testing
+CHANS = ["#ndjr"] # A list, so we can connect to multiple chans
+NICK = "NDJr"
+USER = 'NDJr'
+IDENT = 'NDJr'
+"""
+The bot runs with a user heirchy, of:
+OWNER --> ADMIN --> ALLOWED --> USER --> IGNORED
+All of these are managed via lists
+OWNER can restart, make the bot quit, and anything that would alter it's state
+ADMIN can pause, set allowed, ignored and have bot join & part chans
+USER can interact with the bot,its purpose
+IGNORED will simply have their commands disregarded.
+"""
+ADMIN = []
+ALLOWED = []
+IGNORED = []
+"""
+The bot requires a nick key, in order to identify as a user.
+This should be stored in a file, called: key.txt. It will read the text 
+from there.
+"""
+with open("key.txt", "r") as key_file: #This file is NOT tracked in git :)
+	NICK_KEY = key_file.readlines()[0].strip()
+#Now, connect
+socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print 'BOT IS CONNECTING TO:' + HOST
+socket.connect((HOST, 6697))
+irc = ssl.wrap_socket(socket)
+irc.send("USER " + NICK + " " + NICK + " " + NICK + " :NDJrbot\n")
+irc.send("NICK " + NICK + "\n")
+
+"""
+Now, a loop to receive data
+"""
+while True:
+     # This is the parsing of the message
+	text = irc.recv(4096) # Text, will be the data that we get    
+	"""
+	The first things that need to be checked for, are the PINGS from the server
+	the message of the day, and nickserv PRICMSGs
+	"""
+	
+	if text.find("PING") != -1:
+		irc.send('PONG ' + text.split()[1] + '\r\n')
+		print "PINGED"
+	if text.find("MOTD") != -1:
+		irc.send("PRIVMSG Nickserv :identify {0}\r\n".format(NICK_KEY))
+	if text.find("Password accepted - you are now recognized.") != -1:
+		for chan in CHANS:
+			irc.send('JOIN ' + chan + '\r\n')
+	print text
+	"""
+	Will be adding message parsing, etc in the future.
+	"""
---
+++ ircfunctions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#Filename: irc.py
+"""
+This is a file containing several of the functions that will be used 
+by the irc bot. This includes send <expand as things are added> 
+"""
+
+def bot_send(chan,message):
+	"""
+	This will be the function for sending messages.
+	This takes two arguments, and sends a message, to the chan of the 
+	first argument, and the text of the second.
+	It then prints out something for the console, signifying this event.
+	"""
+	irc.send("PRIVMSG {0} :{1}\r\n".format(chan, message))
+	print "BOT ==> {0} | {1}".format(chan, message)
+	
+
